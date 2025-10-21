bf43a5f87eb4092afe18365d9c262c716876cba9
Tomilola <83653154+Bluel0la@users.noreply.github.com>
2025-10-21 05:43:56 +0100

    Removes API version prefix from router initialization
    Eliminates the hardcoded API version prefix in the router setup to improve flexibility and maintainability.
    
    Adds several dependencies to the requirements file for enhanced functionality, including FastAPI CLI and database support.
    
    Deletes the unused `setup.py` file as part of transitioning to dependency management via `requirements.txt`.

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
 from api.v1.routes.string_analyzer import strings_router
-api_version_one = APIRouter(prefix="/api/v1")
+api_version_one = APIRouter()
 
 api_version_one.include_router(strings_router)
